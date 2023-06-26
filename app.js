a7f94298f1263ed63fa0051f2c60e10cd5123579
BatShevaYamin <hand2b13@mbjcomp.org.il>
2023-06-26 20:01:45 +0300

    more

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const logger = require('./middlewares/logger');
 const app = express();
 
 app.use(express.json());
-app.listen(3000, () => console.log('app is listening at port 3000'));
 //get campaign details
 // app.get('/getCampaignDetails', (req, res) => {
 //     //מקבל את דף המצינג ומציג את הפרטים שלו
@@ -20,5 +19,6 @@ app.use('/collectors', collectors);
 app.use('/details',details);
 app.use('/donations',donations);
 app.use(logger('end'));
+app.listen(3000, () => console.log('app is listening at port 3000'));
 
 
